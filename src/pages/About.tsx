b1971c80909ad732861015220df00302ad1998f0
unused-variable <a.sriramworks@gmail.com>
2025-03-01 16:04:34 +0530

    Update about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -17,9 +17,9 @@ const About = () => {
             <p className="text-lg sm:text-xl text-justify leading-relaxed">
               Avishkar, the flagship event of IEEE - VBIT SB, is held exclusively for the freshmen of VBIT. In 2011, Avishkar was
               awarded the esteemed 'Darrel Chong Gold Student Activity Award' in recognition of organizing Avishkar
-              consecutively for five years. Avishkar offers a platform to present their innovative ideas while improving their
-              technical knowledge and communication skills. Avishkar has played a key role in encouraging research and critical
-              thinking among first year students.
+              consecutively for five years. Avishkar provides a platform to present innovative ideas while improving technical
+              knowledge and communication skills. Avishkar has played a key role in encouraging research and critical thinking
+              among first year students.
             </p>
           </div>
 
@@ -27,27 +27,31 @@ const About = () => {
           <div>
             <h2 className="text-2xl sm:text-3xl font-semibold mb-4 uppercase">What is Avishkar?</h2>
             <p className="text-lg sm:text-xl text-justify leading-relaxed mb-4">
-              Avishkar is a Technical Paper Presentation competition that provides students with the opportunity to address real-
-              world challenges and suggest innovative solutions. Participants will showcase their technical expertise through
-              presentations. This competition plays a key role in the professional development of VBIT freshmen, providing them
-              with essential problem-solving skills and real-world exposure.
+              Avishkar is a Technical Paper Presentation competition that provides students with the opportunity to address real
+              world challenges and suggest innovative solutions, where participants can showcase their technical expertise through
+              presentations. This competition encourages freshmen of VBIT to develop professionally by enhancing their problem-solving
+              and communication skills.
             </p>
             <p className="text-lg sm:text-xl leading-relaxed mb-4">
               It is structured into two rounds:
             </p>
-            <ul className="list-disc pl-8 text-lg sm:text-xl mb-4">
-              <li>Round 1: Idea Presentation Round</li>
-              <li>Round 2: Final Round</li>
-            </ul>
-            <p className="text-lg sm:text-xl text-justify leading-relaxed mb-4">
-              During the initial stage, participants are assigned to designated halls for their presentations. Each team is given a
-              specific amount of time for presenting their ideas and two minutes for the Queries.
-            </p>
-            <p className="text-lg sm:text-xl text-justify leading-relaxed">
-              After Round 2, a panel of judges will evaluate the presentations and select the best participants based on the innovative
-              ideas, technical expertise and presentation skills. The top participants will be recognized for their outstanding
-              contributions.
-            </p>
+            <div className="space-y-4">
+              <div>
+                <h3 className="text-xl font-semibold mb-2">Round 1: Idea Presentation Round</h3>
+                <p className="text-lg sm:text-xl text-justify leading-relaxed">
+                  During the initial stage, participants are assigned to designated halls for their presentations. Each team is given a
+                  specific amount of time for presenting their ideas and two minutes for the Queries.
+                </p>
+              </div>
+              <div>
+                <h3 className="text-xl font-semibold mb-2">Round 2: Final Round</h3>
+                <p className="text-lg sm:text-xl text-justify leading-relaxed">
+                  A panel of judges will evaluate the presentations and select the best participants based on the innovative ideas,
+                  technical expertise and presentation skills. The leading participants will be recognized for their outstanding
+                  contributions.
+                </p>
+              </div>
+            </div>
           </div>
 
           {/* RULES AND REGULATIONS */}
@@ -62,22 +66,21 @@ const About = () => {
               <li>The Problem statement title must be written in bold with a font size of 24.</li>
               <li>The details must include:
                 <ul className="list-decimal pl-8 mt-2">
-                  <li>Name</li>
-                  <li>Branch</li>
-                  <li>Roll number</li>
-                  <li>Email ID</li>
+                  <li>Name.</li>
+                  <li>Branch.</li>
+                  <li>Roll number.</li>
+                  <li>Email ID.</li>
                 </ul>
               </li>
               <li>The style for the details must be italics.</li>
-              <li>All sub-heading must be capitalised with font size of 10.</li>
+              <li>All sub-heading must be capitalized with font size of 10.</li>
               <li>Subheadings must include:
                 <ul className="list-decimal pl-8 mt-2">
-                  <li>Introduction</li>
-                  <li>Problem statement</li>
-                  <li>Motivation</li>
-                  <li>Scope</li>
-                  <li>Proposed Solution</li>
-                  <li>Conclusion</li>
+                  <li>Introduction.</li>
+                  <li>Problem statement.</li>
+                  <li>Motivation.</li>
+                  <li>Scope.</li>
+                  <li>Conclusion.</li>
                 </ul>
               </li>
               <li>The content must be appropriate and should be divided into two columns.</li>
@@ -85,21 +88,35 @@ const About = () => {
               <li>There is no word count for the Abstract, but it should fit in one page.</li>
             </ul>
 
+            {/* IMPORTANT NOTE */}
+            <h3 className="text-xl sm:text-2xl font-semibold mt-6 mb-3">IMPORTANT NOTE:</h3>
+            <ul className="list-disc pl-8 text-lg sm:text-xl space-y-2">
+              <li>After submitting the abstract, you will receive a sample PPT in IEEE format.</li>
+              <li>The abstract must be submitted as a .docx file.</li>
+              <li>Name the abstract file as follows: 'Name_last3digitsofyourRollnumber'.</li>
+              <li>Example:
+                <ul className="list-decimal pl-8 mt-2">
+                  <li>Aneesh_314.docx</li>
+                  <li>Abhirama_112_Lohitha_301.docx</li>
+                </ul>
+              </li>
+            </ul>
+
             {/* PPT */}
             <h3 className="text-xl sm:text-2xl font-semibold mt-6 mb-3">PPT:</h3>
             <ul className="list-disc pl-8 text-lg sm:text-xl space-y-2">
               <li>The PPT should consist of 10 slides, covering the following topics:
                 <ul className="list-decimal pl-8 mt-2">
-                  <li>Slide 1: Home Page</li>
-                  <li>Slide 2: Problem Statement</li>
-                  <li>Slide 3: Problem Explanation</li>
-                  <li>Slide 4: Solution</li>
-                  <li>Slide 5: Domain Chosen</li>
-                  <li>Slide 6: Feasibility of the Solution</li>
-                  <li>Slide 7: Comparing with Existing Solutions</li>
-                  <li>Slide 8: Applications</li>
-                  <li>Slide 9: Queries</li>
-                  <li>Slide 10: Thank You</li>
+                  <li>Slide 1: Home Page.</li>
+                  <li>Slide 2: Problem Statement.</li>
+                  <li>Slide 3: Problem Explanation.</li>
+                  <li>Slide 4: Solution.</li>
+                  <li>Slide 5: Domain Chosen.</li>
+                  <li>Slide 6: Feasibility of the Solution.</li>
+                  <li>Slide 7: Comparing with Existing Solutions.</li>
+                  <li>Slide 8: Applications.</li>
+                  <li>Slide 9: Queries.</li>
+                  <li>Slide 10: Thank You.</li>
                 </ul>
               </li>
               <li>The homepage must consist of "AVISHKAR" along with your Name, Roll Number and Branch & Section.</li>
@@ -108,30 +125,14 @@ const About = () => {
               <li>No animations or transitions should be used in the PPT.</li>
               <li>The subheadings of the slide 2, slide 3, slide 4, slide 5, slide 6, slide 7 and slide 8 must be in font size 40 and the content font size should be 20.</li>
               <li>Use the same file naming format for the PPT as you did for the abstract.</li>
+              <li>Submit the Abstract and PPT before the given deadlines.</li>
+              <li>Example:
+                <ul className="list-decimal pl-8 mt-2">
+                  <li>Lohitha_314.pptx</li>
+                  <li>Aneesh_112_Shreshta_3A1.pptx</li>
+                </ul>
+              </li>
             </ul>
-
-            {/* IMPORTANT NOTE */}
-            <div className="mt-6">
-              <h3 className="text-xl sm:text-2xl font-semibold mb-3">IMPORTANT NOTE:</h3>
-              <ul className="list-disc pl-8 text-lg sm:text-xl space-y-2">
-                <li>After submitting the abstract, you will receive a sample PPT in IEEE format.</li>
-                <li>The abstract must be submitted as a .docx file.</li>
-                <li>Name the abstract file as follows: 'Name_last3digitsofyourRollnumber'.</li>
-                <li>Example file names for Abstract:
-                  <ul className="list-decimal pl-8 mt-2">
-                    <li>Aneesh_314.docx</li>
-                    <li>Abhirama_112_Lohitha_301.docx</li>
-                  </ul>
-                </li>
-                <li>Example file names for PPT:
-                  <ul className="list-decimal pl-8 mt-2">
-                    <li>Lohitha_314.pptx</li>
-                    <li>Aneesh_112_Shreshta_3A1.pptx</li>
-                  </ul>
-                </li>
-                <li className="font-semibold mt-4">Both the Abstract and PPT must be submitted by the provided deadline.</li>
-              </ul>
-            </div>
           </div>
         </div>
       </div>
